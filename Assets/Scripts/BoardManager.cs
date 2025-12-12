7335a547d6a7c00a719267439cd4dd4ed4cc8568
zinine73 <zinine@naver.com>
2025-12-12 13:26:06 +0900

    add food

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -14,16 +15,18 @@ public class BoardManager : MonoBehaviour
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
     public PlayerController Player;
+    public GameObject FoodPrefab;
 
     Tilemap tilemap;
     CellData[,] boardData;
     Grid grid;
+    List<Vector2Int> emptyCellList;
 
     public void Init()
     {
         tilemap = GetComponentInChildren<Tilemap>();
         grid = GetComponentInChildren<Grid>();
-
+        emptyCellList = new List<Vector2Int>();
         boardData = new CellData[Width, Height];
 
         for (int y = 0; y < Height; y++)
@@ -34,20 +37,20 @@ public class BoardManager : MonoBehaviour
                 boardData[x, y] = new CellData();
                 if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
                 {
-                    //tile = WallTiles[Random.Range(0, WallTiles.Length)];
                     tile = GetRandomTile(WallTiles);
                     boardData[x, y].Passable = false;
                 }
                 else
                 {
-                    //tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
                     tile = GetRandomTile(GroundTiles);
                     boardData[x, y].Passable = true;
+                    emptyCellList.Add(new Vector2Int(x, y));
                 }
                 tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
-        //Player.Spawn(this, new Vector2Int(1, 1));
+        emptyCellList.Remove(new Vector2Int(1, 1));
+        GenerateFood();
     }
 
     Tile GetRandomTile(Tile[] tiles)
@@ -68,5 +71,23 @@ public class BoardManager : MonoBehaviour
             return null;
         }    
         return boardData[cellIndex.x, cellIndex.y];
+    }
+
+    void GenerateFood()
+    {
+        int foodCount = 5;
+        for (int i = 0; i < foodCount; i++)
+        {
+            //int randomX = Random.Range(1, Width - 1);
+            //int randomY = Random.Range(1, Height - 1);
+            int randomIndex = Random.Range(0, emptyCellList.Count);
+            Vector2Int coord = emptyCellList[randomIndex];
+
+            emptyCellList.RemoveAt(randomIndex);
+            CellData data = boardData[coord.x, coord.y];
+            GameObject newFood = Instantiate(FoodPrefab);
+            newFood.transform.position = CellToWorld(coord);
+            data.ContainedObject = newFood;
+        }
     }
 }
